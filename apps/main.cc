dbc11cc56d6e13265b2000bef1f9a0539f1fd5e7
Christian Ermann <christianermann@gmail.com>
2020-09-27 18:56:03 -0700

    Added error checking to initialization.

--- apps/main.cc
+++ apps/main.cc
@@ -6,26 +6,60 @@
 
 const GLint WIDTH = 512, HEIGHT = 512;
 
+
+// OpenGL debugging info 
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
+        GLenum severity, GLsizei length, const GLchar* message,
+        const void* userParam)
+{
+    fprintf(stderr,
+            "GL CALLBACK: %s type = 0x%x, severity = 0x%xm nessage = %s\n",
+            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type,
+            severity, message);
+}
+
+
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-            SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        std::cerr << "SDL INIT ERROR: " << SDL_GetError() << std::endl;
+    }
 
     SDL_Window *window = SDL_CreateWindow("PolyFy", 100, 100, WIDTH, HEIGHT,
             SDL_WINDOW_OPENGL);
+
+    if (window == NULL)
+    {
+        std::cerr << "SDL WINDOW ERROR: " << SDL_GetError() << std::endl;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+            SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
-    if (glewInit() != GLEW_OK)
+    if (context == NULL)
     {
-        std::cout << "Failed to initialize GLEW!" << std::endl;
+        std::cerr << "SDL GL CONTEXT ERROR: " << SDL_GetError() << std::endl;
+    }
+ 
+    glewExperimental = GL_TRUE;
+
+    GLenum glew_status = glewInit();
+    if (glew_status != GLEW_OK)
+    {
+        std::cerr << "GLEW ERROR: " << glewGetErrorString(glew_status)
+            << std::endl;
         return EXIT_FAILURE;
     }
 
     glViewport(0, 0, WIDTH, HEIGHT);
+    
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
 
     SDL_Event windowEvent;
     while (true)
